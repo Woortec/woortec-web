816841723f6fceb0551f60cfa8358abc6a65a5ca
Álvaro <Alvaro@woortec.com>
2026-05-15 16:09:10 +0000

    Add Microsoft Clarity tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,6 +83,18 @@ export default async function RootLayout({
     <html lang={region.locale}>
       <head>
         <GoogleAnalytics />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "wrkf27qmr0");
+            `,
+          }}
+        />
         <OrganizationJsonLd />
         <WebsiteJsonLd />
       </head>
